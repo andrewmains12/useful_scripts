4bd4c2e0bccb481dd42b18d6d604c4ae8ae8e00e
Andrew Mains <andrew.mains@upsight.com>
2015-04-11 16:09:34 -0700

    Add merge git repos script

--- merge_git_repos.sh
+++ merge_git_repos.sh
@@ -1,32 +1,35 @@
+#!/bin/bash
 set -e
-REPOS="valhalla-core valhalla-message-loader valhalla-derived-updater valkyrie"
-BRANCHES="master develop"
+
+# Found this; not sure if it works. Probably largely copypasted off the internet.
+
+if [[ $# -lt 1 ]]; then
+  echo "Usage: merge_git_repos.sh <repos> [branches]"
+  echo "Merge branches (default master, develop) from repos into this repo."
+  exit 1
+fi
+
+REPOS="${1}"
+BRANCHES="${2-master develop}"
 
 function merge_repo {
-    repo_name=$1
-    repo_url=$2
-    branch=$3
+    repo_url=$1
+    branch=$2
+    repo_name="$(basename repo_url)"
+    repo_name="${repo_name%.*}"
 
     if [ -z `git config --get remote.$repo_name.url` ]; then
-	git remote add -f $repo_name $repo_url
+	git remote add -f "${repo_name}" "${repo_url}"
     fi
 
     branch_spec=$repo_name/$branch
     git merge -s ours --no-commit $branch_spec
     git read-tree --prefix=$repo_name -u $branch_spec
-    git commit -m "$repo_name: merged in $repo_name into valhalla project"
+    git commit -m "$repo_name: merged in $repo_name"
 }
-
-function get_remote {
-    pushd . > /dev/null
-    cd ../valhalla-parent/$1
-    git config --get remote.origin.url
-    popd > /dev/null
-}
-
 
 for repo in $REPOS; do
     for branch in $BRANCHES; do
-	merge_repo $repo `get_remote $repo` $branch
+	merge_repo "${repo}" "${branch}"
     done
 done
